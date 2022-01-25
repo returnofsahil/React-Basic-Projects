31b3cabf12a4af916fd955f39765296aa59e5931
Sahil Bhandari <returnofsahil@gmail.com>
2022-01-25 20:50:06 +0530

    1st project done

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+  //func
+  const removeHandler = () => {
+    setPeople([]);
+  };
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthday today</h3>
+        <List people={people} />
+        <button onClick={removeHandler}>clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
