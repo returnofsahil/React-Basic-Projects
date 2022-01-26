1e59da546398fe6ddef63313de28f0ff9c3532cc
Sahil Bhandari <returnofsahil@gmail.com>
2022-01-26 15:08:59 +0530

    2nd project complete post/tours one

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, title, body, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+
+  return (
+    <article className="single-tour">
+      <div className="tour-info">
+        <h4>{title}</h4>
+      </div>
+      <p>
+        {readMore ? body : `${body.substring(0, 50)}...`}
+        <button onClick={() => setReadMore(!readMore)}>
+          {readMore ? "show less" : "Read More"}
+        </button>
+      </p>
+      <button
+        className="delete-btn"
+        onClick={() => {
+          removeTour(id);
+        }}
+      >
+        Not intereseted
+      </button>
+    </article>
+  );
 };
 
 export default Tour;
--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,19 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>our posts</h2>
+        <div className="underline"></div>
+        <div>
+          {tours.map((tour) => {
+            return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
