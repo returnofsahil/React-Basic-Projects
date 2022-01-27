a825252a6204bd87dc17ea4be170af97ee2b9d9b
Sahil Bhandari <returnofsahil@gmail.com>
2022-01-28 00:10:36 +0530

    4th done fix the key prop

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,25 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {data.map((item) => {
+            return (
+              <SingleQuestion
+                id={item.id}
+                title={item.title}
+                info={item.info}
+              />
+            );
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,20 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ title, info, id }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  return (
+    <article className="question" key={id}>
+      <header>
+        <h4 key={id}>{title}</h4>
+        <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p key={id}>{info}</p>}
+    </article>
+  );
 };
+
+//now able to transfer data
 
 export default Question;
